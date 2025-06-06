6bfc93b25e0dda288910b567ae6e264ad363e718
yogaooo <89327025@qq.com>
2025-06-06 12:04:25 +0800

    修改替换API地址

--- src/common.rs
+++ src/common.rs
@@ -996,7 +996,7 @@ fn get_api_server_(api: String, custom: String) -> String {
             return format!("http://{}", s);
         }
     }
-    "https://admin.rustdesk.com".to_owned()
+    "http://10.79.1.169:21114".to_owned()
 }
 
 #[inline]
